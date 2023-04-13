4cde5510e0665a209378174634a74842c6a1aef3
iagoomes <iagoomes@outlook.com>
2023-04-12 22:20:39 -0300

    Bean validation + Cadastro produto + Estilização fomulario de cadastro

--- mudi/mudi/src/main/java/br/com/iagoomes/mudi/controller/PedidoController.java
+++ mudi/mudi/src/main/java/br/com/iagoomes/mudi/controller/PedidoController.java
@@ -1,4 +1,41 @@
 package br.com.iagoomes.mudi.controller;
 
+import br.com.iagoomes.mudi.dto.PedidoDto;
+import br.com.iagoomes.mudi.model.Pedido;
+import br.com.iagoomes.mudi.repository.PedidoRepository;
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+
+@Controller
+@RequestMapping("pedido")
 public class PedidoController {
+
+    private final PedidoRepository repository;
+
+    public PedidoController(PedidoRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("formulario")
+    public String formulario() {
+        return "pedido/novo-pedido";
+    }
+
+    @PostMapping("novo")
+    public String novo(@Valid PedidoDto requesicao, BindingResult result) {
+        if (result.hasErrors()) {
+            return "pedido/novo-pedido";
+        }
+        Pedido pedido = requesicao.toPedido();
+        repository.save(pedido);
+
+        return "pedido/novo-pedido";
+    }
+
 }
---
+++ mudi/mudi/src/main/java/br/com/iagoomes/mudi/dto/PedidoDto.java
@@ -0,0 +1,56 @@
+package br.com.iagoomes.mudi.dto;
+
+import br.com.iagoomes.mudi.model.Pedido;
+import org.hibernate.validator.constraints.NotBlank;
+
+public class PedidoDto {
+    @NotBlank
+    private String nomeProduto;
+    @NotBlank
+    private String urlProduto;
+    @NotBlank
+    private String urlImagem;
+    private String descricao;
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public String getUrlProduto() {
+        return urlProduto;
+    }
+
+    public void setUrlProduto(String urlProduto) {
+        this.urlProduto = urlProduto;
+    }
+
+    public String getUrlImagem() {
+        return urlImagem;
+    }
+
+    public void setUrlImagem(String urlImagem) {
+        this.urlImagem = urlImagem;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Pedido toPedido() {
+        Pedido pedido = new Pedido();
+        pedido.setDescricao(descricao);
+        pedido.setNomeProduto(nomeProduto);
+        pedido.setUrlProduto(urlProduto);
+        pedido.setUrlImagem(urlImagem);
+
+        return pedido;
+    }
+}
